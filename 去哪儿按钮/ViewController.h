f093fef3202826de4cdb503f14cced37f7b0e0a9
杨华 <593283083@qq.com>
2015-07-14 14:12:35 +0800

    Initial Commit

---
+++ 去哪儿按钮/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  去哪儿按钮
+//
+//  Created by xxzx on 15/7/14.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 去哪儿按钮/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  去哪儿按钮
+//
+//  Created by xxzx on 15/7/14.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
