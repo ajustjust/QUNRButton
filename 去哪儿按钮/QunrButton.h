655c0c865bf3d0dc80601cc1d6d7d143479cbf9d
杨华 <593283083@qq.com>
2015-08-11 08:59:50 +0800

    第一次提交

---
+++ 去哪儿按钮/QunrButton.h
@@ -0,0 +1,13 @@
+//
+//  Qubutton.h
+//  去哪儿按钮
+//
+//  Created by xxzx on 15/7/14.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Qubutton : UIButton
+
+@end
---
+++ 去哪儿按钮/QunrButton.m
@@ -0,0 +1,21 @@
+//
+//  Qubutton.m
+//  去哪儿按钮
+//
+//  Created by xxzx on 15/7/14.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import "Qubutton.h"
+
+@implementation Qubutton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
